21548d41711c7d08a988a72c4023e294d38a23fd
Isabel Lamim <61071369+ilamim@users.noreply.github.com>
2021-11-04 00:41:49 +0100

    added atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,18 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilamim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/03 20:37:37 by ilamim            #+#    #+#             */
-/*   Updated: 2021/11/03 21:07:12 by ilamim           ###   ########.fr       */
+/*   Created: 2021/11/03 23:53:51 by ilamim            #+#    #+#             */
+/*   Updated: 2021/11/04 00:39:42 by ilamim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isprint(int c)
+int	ft_isspace(int c)
 {
-	return (c >= 32 && c <= 127);
+	return (c == 32 || (c >= 9 && c <= 13));
+}
+
+int	ft_atoi(const char *str)
+{
+	int	num;
+	int	sign;
+
+	num = 0;
+	sign = 1;
+	while (ft_isspace((int)*str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -sign;
+		str++;
+	}
+	while (ft_isdigit((int)*str))
+	{
+		num = num * 10 + *str - '0';
+		str++;
+	}
+	return (sign * num);
 }
