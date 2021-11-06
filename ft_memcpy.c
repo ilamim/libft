ac7f7dc4460f0e5ebd7bcb484a6ef84a8302adc0
Isabel L <lamim@protonmail.com>
2021-11-06 16:36:09 +0100

    added functional draft of memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+void *ft_memcpy(void *dest, void *src, size_t n)
+{
+	unsigned char *cdest;
+	unsigned char *csrc;
+
+	cdest = (unsigned char *)dest;
+	csrc = (unsigned char *)src;
+	while (n > 0)
+	{
+		*(cdest++) = *(csrc++);
+		n--;
+	}
+	return (dest);
+}
+
+int main()
+{
+	char dest[30];
+	
+	ft_memcpy(dest, "totototototo", 12);
+	printf("%s", dest);
+	return 0;
+}	
